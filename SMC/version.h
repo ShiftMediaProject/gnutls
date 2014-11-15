b95bc6312309ba481c6b61804fbb854323976c65
Matthew Oliver <protogonoi@gmail.com>
2014-11-15 18:30:03 +1100

    Update VS project files for recent merge.

--- SMC/version.h
+++ SMC/version.h
@@ -1,8 +1,8 @@
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GnuTLS 3.2.19"
+#define PACKAGE_STRING "GnuTLS 3.2.20"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.2.19"
+#define PACKAGE_VERSION "3.2.20"
 
 /* Version number of package */
-#define VERSION "3.2.19"
+#define VERSION "3.2.20"
