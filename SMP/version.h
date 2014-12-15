6d7dc464976531a97c71044487c49e3738d0784c
Matt Oliver <protogonoi@gmail.com>
2014-12-15 20:36:28 +1100

    project: Update for recent merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,8 +1,8 @@
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GnuTLS 3.2.20"
+#define PACKAGE_STRING "GnuTLS 3.2.21"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.2.20"
+#define PACKAGE_VERSION "3.2.21"
 
 /* Version number of package */
-#define VERSION "3.2.20"
+#define VERSION "3.2.21"
