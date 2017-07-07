6bc19037c39d3cbf8e31661973e36e1ab336663c
Matt Oliver <protogonoi@gmail.com>
2017-07-07 22:20:44 +1000

    project: Update for 3.5.14 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,5 +1,5 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.5.13"
+#define PACKAGE_VERSION "3.5.14"
 
 /* Version number of package */
-#define VERSION "3.5.13"
+#define VERSION "3.5.14"
