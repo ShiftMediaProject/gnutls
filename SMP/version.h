ba3d806e11b12b540086128547896a4441aa666b
Matt Oliver <protogonoi@gmail.com>
2018-02-18 19:39:38 +1100

    project: Update for 3.5.18 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,5 +1,5 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.5.17"
+#define PACKAGE_VERSION "3.5.18"
 
 /* Version number of package */
-#define VERSION "3.5.17"
+#define VERSION "3.5.18"
