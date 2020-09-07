68bd1fa3f09765e215b82ee75f4f1e5d29023b86
Matt Oliver <protogonoi@gmail.com>
2020-09-07 20:19:00 +1000

    project: Update for 3.6.15 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,5 +1,5 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.6.14"
+#define PACKAGE_VERSION "3.6.15"
 
 /* Version number of package */
-#define VERSION "3.6.14"
+#define VERSION "3.6.15"
