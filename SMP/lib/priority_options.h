5aca975f99ab31652f6d09af70ee3177d283d425
Matt Oliver <protogonoi@gmail.com>
2021-06-26 18:44:22 +1000

    project: Update for 3.7.2 merge.

--- SMP/lib/priority_options.h
+++ SMP/lib/priority_options.h
@@ -35,12 +35,12 @@ typedef void (*option_set_func)(gnutls_priority_t);
 #line 6 "lib/priority_options.gperf"
 struct priority_options_st { const char *name; option_set_func func; };
 
-#define TOTAL_KEYWORDS 34
+#define TOTAL_KEYWORDS 35
 #define MIN_WORD_LENGTH 6
 #define MAX_WORD_LENGTH 32
 #define MIN_HASH_VALUE 6
-#define MAX_HASH_VALUE 52
-/* maximum key range = 47, duplicates = 0 */
+#define MAX_HASH_VALUE 57
+/* maximum key range = 52, duplicates = 0 */
 
 #ifdef __GNUC__
 __inline
@@ -54,32 +54,32 @@ hash (register const char *str, register size_t len)
 {
   static const unsigned char asso_values[] =
     {
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 25,
-      30, 53, 53,  5, 53, 53,  5, 53, 53, 53,
-      53, 53, 53, 53, 53,  5, 53, 35,  0, 10,
-       0, 30,  0, 53, 53,  0, 15,  0,  0, 53,
-       0, 53, 53, 10,  5,  5,  0,  5, 53, 25,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53, 53, 53, 53, 53,
-      53, 53, 53, 53, 53, 53
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 15,
+      25, 58, 58,  5, 58, 58, 20, 58, 58, 58,
+      58, 58, 58, 58, 58, 20, 58, 10,  0,  5,
+       0,  0,  5, 58, 58, 10, 20,  0,  0, 58,
+      10, 58, 58,  0,  0,  5, 15, 30, 58, 20,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58, 58, 58, 58, 58,
+      58, 58, 58, 58, 58, 58
     };
   return len + asso_values[(unsigned char)str[len - 1]] + asso_values[(unsigned char)str[0]];
 }
@@ -92,51 +92,55 @@ static const struct priority_options_st wordlist[] =
     {""}, {""},
 #line 13 "lib/priority_options.gperf"
     {"FORCE_ETM", enable_force_etm},
-    {""},
-#line 9 "lib/priority_options.gperf"
-    {"DUMBFW", enable_dumbfw},
-#line 34 "lib/priority_options.gperf"
-    {"PROFILE_HIGH", enable_profile_high},
-#line 27 "lib/priority_options.gperf"
-    {"FALLBACK_SCSV", enable_fallback_scsv},
-#line 33 "lib/priority_options.gperf"
-    {"PROFILE_MEDIUM", enable_profile_medium},
-#line 14 "lib/priority_options.gperf"
-    {"NO_SESSION_HASH", enable_no_ext_master_secret},
-#line 31 "lib/priority_options.gperf"
-    {"PROFILE_LOW", enable_profile_low},
-#line 30 "lib/priority_options.gperf"
-    {"PROFILE_VERY_WEAK", enable_profile_very_weak},
-#line 35 "lib/priority_options.gperf"
-    {"PROFILE_ULTRA", enable_profile_ultra},
-#line 16 "lib/priority_options.gperf"
-    {"VERIFY_ALLOW_BROKEN", enable_verify_allow_broken},
 #line 11 "lib/priority_options.gperf"
     {"NO_TICKETS", enable_no_tickets},
-#line 25 "lib/priority_options.gperf"
-    {"PARTIAL_RENEGOTIATION", enable_partial_safe_renegotiation},
-#line 37 "lib/priority_options.gperf"
-    {"PROFILE_SUITEB128", enable_profile_suiteb128},
+#line 39 "lib/priority_options.gperf"
+    {"NEW_PADDING", dummy_func},
+    {""},
 #line 10 "lib/priority_options.gperf"
     {"NO_EXTENSIONS", enable_no_extensions},
-#line 36 "lib/priority_options.gperf"
-    {"PROFILE_FUTURE", enable_profile_future},
-#line 23 "lib/priority_options.gperf"
-    {"UNSAFE_RENEGOTIATION", enable_unsafe_renegotiation},
-#line 26 "lib/priority_options.gperf"
-    {"DISABLE_SAFE_RENEGOTIATION", disable_safe_renegotiation},
+    {""}, {""},
+#line 8 "lib/priority_options.gperf"
+    {"COMPAT", enable_compat},
 #line 28 "lib/priority_options.gperf"
     {"DISABLE_WILDCARDS", disable_wildcards},
 #line 24 "lib/priority_options.gperf"
     {"SAFE_RENEGOTIATION", enable_safe_renegotiation},
 #line 20 "lib/priority_options.gperf"
     {"SSL3_RECORD_VERSION", enable_ssl3_record_version},
-#line 17 "lib/priority_options.gperf"
-    {"VERIFY_ALLOW_SIGN_RSA_MD5", enable_verify_allow_rsa_md5},
+#line 14 "lib/priority_options.gperf"
+    {"NO_SESSION_HASH", enable_no_ext_master_secret},
 #line 15 "lib/priority_options.gperf"
     {"STATELESS_COMPRESSION", dummy_func},
-#line 22 "lib/priority_options.gperf"
-    {"VERIFY_ALLOW_X509_V1_CA_CRT", dummy_func},
+#line 29 "lib/priority_options.gperf"
+    {"SERVER_PRECEDENCE", enable_server_precedence},
+    {""},
+#line 33 "lib/priority_options.gperf"
+    {"PROFILE_MEDIUM", enable_profile_medium},
+#line 23 "lib/priority_options.gperf"
+    {"UNSAFE_RENEGOTIATION", enable_unsafe_renegotiation},
+#line 26 "lib/priority_options.gperf"
+    {"DISABLE_SAFE_RENEGOTIATION", disable_safe_renegotiation},
+#line 34 "lib/priority_options.gperf"
+    {"PROFILE_HIGH", enable_profile_high},
+#line 27 "lib/priority_options.gperf"
+    {"FALLBACK_SCSV", enable_fallback_scsv},
+#line 36 "lib/priority_options.gperf"
+    {"PROFILE_FUTURE", enable_profile_future},
+#line 42 "lib/priority_options.gperf"
+    {"DISABLE_TLS13_COMPAT_MODE", disable_tls13_compat_mode},
+#line 25 "lib/priority_options.gperf"
+    {"PARTIAL_RENEGOTIATION", enable_partial_safe_renegotiation},
+#line 40 "lib/priority_options.gperf"
+    {"DEBUG_ALLOW_KEY_USAGE_VIOLATIONS", enable_server_key_usage_violations},
+    {""},
+#line 16 "lib/priority_options.gperf"
+    {"VERIFY_ALLOW_BROKEN", enable_verify_allow_broken},
+    {""},
+#line 9 "lib/priority_options.gperf"
+    {"DUMBFW", enable_dumbfw},
+#line 30 "lib/priority_options.gperf"
+    {"PROFILE_VERY_WEAK", enable_profile_very_weak},
     {""},
 #line 41 "lib/priority_options.gperf"
     {"ALLOW_SMALL_RECORDS", enable_allow_small_records},
@@ -144,19 +148,20 @@ static const struct priority_options_st wordlist[] =
     {"VERIFY_DISABLE_CRL_CHECKS", disable_crl_checks},
 #line 21 "lib/priority_options.gperf"
     {"LATEST_RECORD_VERSION", enable_latest_record_version},
-#line 29 "lib/priority_options.gperf"
-    {"SERVER_PRECEDENCE", enable_server_precedence},
-    {""},
+#line 22 "lib/priority_options.gperf"
+    {"VERIFY_ALLOW_X509_V1_CA_CRT", dummy_func},
+#line 35 "lib/priority_options.gperf"
+    {"PROFILE_ULTRA", enable_profile_ultra},
 #line 32 "lib/priority_options.gperf"
     {"PROFILE_LEGACY", enable_profile_legacy},
+#line 17 "lib/priority_options.gperf"
+    {"VERIFY_ALLOW_SIGN_RSA_MD5", enable_verify_allow_rsa_md5},
     {""},
-#line 39 "lib/priority_options.gperf"
-    {"NEW_PADDING", dummy_func},
-#line 40 "lib/priority_options.gperf"
-    {"DEBUG_ALLOW_KEY_USAGE_VIOLATIONS", enable_server_key_usage_violations},
+#line 37 "lib/priority_options.gperf"
+    {"PROFILE_SUITEB128", enable_profile_suiteb128},
     {""}, {""}, {""},
-#line 8 "lib/priority_options.gperf"
-    {"COMPAT", enable_compat},
+#line 31 "lib/priority_options.gperf"
+    {"PROFILE_LOW", enable_profile_low},
 #line 38 "lib/priority_options.gperf"
     {"PROFILE_SUITEB192", enable_profile_suiteb192},
     {""}, {""}, {""}, {""},
