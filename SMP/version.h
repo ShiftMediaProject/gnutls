4023ab17b2f6d80598f781f0b4e7746cd5acc8d3
Matt Oliver <protogonoi@gmail.com>
2022-09-03 00:57:24 +1000

    project: Update for 3.7.7 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,5 +1,5 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.7.6"
+#define PACKAGE_VERSION "3.7.7"
 
 /* Version number of package */
-#define VERSION "3.7.6"
+#define VERSION "3.7.7"
