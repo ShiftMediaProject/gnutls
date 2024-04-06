23b07fd47ccf09b09f7d4fc2164a12411676b45f
Matt Oliver <protogonoi@gmail.com>
2024-04-06 21:50:55 +1100

    project: Update for 3.8.3 merge.

--- SMP/version.h
+++ SMP/version.h
@@ -1,5 +1,5 @@
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.8.2"
+#define PACKAGE_VERSION "3.8.3"
 
 /* Version number of package */
-#define VERSION "3.8.2"
+#define VERSION "3.8.3"
